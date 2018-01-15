d164fd283d5f1c0888817205608472dabd36d219
Tony Hirst <tony.hirst@gmail.com>
2018-01-15 14:04:41 +0000

    Add a cli

---
+++ pd_datareader_nhs/cli.py
@@ -0,0 +1,12 @@
+import pd_datareader_nhs.nhs_digital_ods as ods
+import click
+import sqlite3
+
+@click.command()
+@click.option('--dbname', default='nhs_ods.db',  help='SQLite database name')
+@click.argument('command')
+def cli(dbname,command):
+	click.echo('Using SQLite3 database: {}'.format(dbname))
+	if command == 'collect':
+		ods.init(sqlite3db=dbname)
+		ods.updatedb()
